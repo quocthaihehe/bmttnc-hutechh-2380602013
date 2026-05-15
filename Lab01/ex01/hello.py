83fc6f5abdbe11d39042e27556a43bcc3fd7c162
quocthaihehe <quocthaihehe@gmail.com>
2026-05-15 09:04:09 +0700

    lab01 hello world

---
+++ Lab01/ex01/hello.py
@@ -0,0 +1,2 @@
+print("Hello world !!")
+print("Xin chào các bạn, mình là Hồ Quốc Thái - mã số sinh viên 2380602013")
---
+++ Lab01/ex02/ex02_01.py
@@ -0,0 +1,6 @@
+# nhập tên và tủi của bạn   
+ten = input("Nhập tên của bạn: ")
+tuoi = input("Nhập tuổi của bạn: ")
+
+#In thông điệp chào mừng với thông tin đăng nhập
+print("Chào mừng " + ten + "! Bạn " + tuoi + " tuổi.")
